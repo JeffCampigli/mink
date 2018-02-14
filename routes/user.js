061387103b244ab24a26fb93df474681e5b04902
bentchaw <tanrithy.rt@gmail.com>
2018-02-14 18:06:00 +0100

    display matched candidat

--- routes/user.js
+++ routes/user.js
@@ -1,16 +1,27 @@
 const express = require("express");
 const router = express.Router();
 const Match = require("../models/match");
+const User = require("../models/user");
 const { ensureLoggedIn } = require("connect-ensure-login");
 
-const User = require("../models/user");
-const Match = require("../models/match");
 const SKILLS = require("../config/user-skills");
 
 router.get("/coach", ensureLoggedIn(), function(req, res, next) {
-  let coachId = req.user.id;
-  const matchedCandidate = Match.find({ coachID });
-  res.render("coach/index", {});
+  //console.log(req.user.id);
+  Match.find({ userCoach: req.user.id }, (err, matched) => {
+    if (err) return next(err);
+    //console.log(matched[0].userCandidat);
+    //res.render("coach/index", { toto: matched[0].userCandidat });
+
+    //console.log(matched[0].userCandidat);
+    let candidatId = matched[0].userCandidat;
+
+    User.find({ _id: candidatId }, (err, candidatMatched) => {
+      if (err) return next(err);
+      console.log(candidatMatched[0]);
+      res.render("coach/index", { match: candidatMatched });
+    });
+  });
 });
 
 router.get("/candidat/:id", ensureLoggedIn(), function(req, res, next) {
